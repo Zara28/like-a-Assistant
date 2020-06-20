d773bad9ffbd41f0d16285cf8f5f39f6c0f59b75
Alena <a.forgot@mail.ru>
2020-06-20 21:56:21 +0300

    fall done

--- test_var_1/Program.cs
+++ test_var_1/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Win32;
+using System.Security.Permissions;
 
 namespace test_var_1
 {
@@ -10,11 +12,19 @@ namespace test_var_1
     {
         static void Main(string[] args)
         {
-             while(Console.ReadKey().Key!= ConsoleKey.Enter)
-             {
-                 Console.WriteLine("Hello world!");
-             }  
-            Console.WriteLine("Hello world!");
+            List <string> comm = new List<string>();
+            RegistryKey key;
+            key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Unistall");
+            foreach(String keyName in key.GetSubKeyNames())
+            {
+                RegistryKey subkey = key.OpenSubKey(keyName);
+                comm.Add(subkey.GetValue("DisplayName") as string);
+            }
+            foreach(string a in comm)
+            {
+                Console.WriteLine(a);
+               
+            }
         }
     }
 }
