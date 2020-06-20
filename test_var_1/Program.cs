e3932ade83fd8acf022810f23a25ec00da930167
Alena <a.forgot@mail.ru>
2020-06-20 21:10:29 +0300

    base

---
+++ test_var_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_var_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             while(Console.ReadKey().Key!= ConsoleKey.Enter)
+             {
+                 Console.WriteLine("Hello world!");
+             }  
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
